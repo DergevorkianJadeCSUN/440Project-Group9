46ecb77e96d66f094b6803c6bf7d9b5c6d6f240e
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2025-03-10 18:27:15 -0700

    refactored. made thigns actually work

--- setup.sql
+++ setup.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE IF NOT EXISTS `project-db`;
-USE `project-db`;
-CREATE TABLE IF NOT EXISTS `user` (
-	`username` varchar(50) NOT NULL,
-    `password` varchar(50) NOT NULL,
-    `firstName` varchar(50) NOT NULL,
-    `lastName` varchar(50),
-    `email` varchar(50) NOT NULL UNIQUE,
-    `phone` varchar(30) NOT NULL UNIQUE
+CREATE DATABASE IF NOT EXISTS projectdb;
+USE projectdb;
+CREATE TABLE IF NOT EXISTS user (
+	username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    firstName varchar(50) NOT NULL,
+    lastName varchar(50),
+    email varchar(50) NOT NULL UNIQUE,
+    phone varchar(30) NOT NULL UNIQUE
 )
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from . import app
+from webapp import app
 
 @app.route("/")
 def home():
