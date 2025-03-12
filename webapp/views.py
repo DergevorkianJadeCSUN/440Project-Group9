965c5a952412ffd399fca438fe32ddb0901cb302
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2025-03-11 17:56:35 -0700

    set up basic login and registration pages (nonfunctional)

--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,18 @@ from flask import Flask
 from flask import render_template
 from webapp import app
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+
+    return render_template("login.html", msg = msg)
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    msg = ''
+
+    return render_template("register.html", msg = msg)
