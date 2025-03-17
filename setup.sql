b49d7dbf7b24600dce435e411c4fbdb4d07b971a
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2025-03-17 11:48:26 -0700

    Updated Setup to be more comprehensive, included dummy user

--- setup.sql
+++ setup.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE IF NOT EXISTS projectdb;
 USE projectdb;
 CREATE TABLE IF NOT EXISTS user (
-	username varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(50),
-    email varchar(50) NOT NULL UNIQUE,
-    phone varchar(30) NOT NULL UNIQUE
-)
+	username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255),
+    email varchar(255) NOT NULL UNIQUE,
+    phone varchar(255) NOT NULL UNIQUE,
+    PRIMARY KEY (username)
+);
+INSERT INTO user VALUES ('dummy','$2b$12$OppYXy/.5p0NuGSIQ9E./ecmlbarsYDakKb2hrF5nRw2KA8uF.XyW','Fake','Name','fakeaddress@domain.tld','1-800-555-5555');
