1699e901c54b309d71f78c03a0b599e7f73c872f
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2025-03-31 10:42:26 -0700

    changed primary key from id to username

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -27,8 +27,8 @@ def create_app():
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+    def load_user(username):
+        return User.query.get(username)
 
     return app
 
--- webapp/models.py
+++ webapp/models.py
@@ -3,8 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), primary_key=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     phone = db.Column(db.String(15), unique=True, nullable=False)
@@ -16,3 +15,6 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def get_id(self):
+        return self.username
