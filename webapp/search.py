19d93922a1902b2b02a3b25437c9a84b9bdefef2
DergevorkianJadeCSUN <jade.dergevorkian.125@my.csun.edu>
2025-05-03 13:14:52 -0700

    Added date filtering to user search

--- webapp/search.py
+++ webapp/search.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required
-from sqlalchemy import desc, select, not_, or_
+from sqlalchemy import desc, select, not_, or_, func
 
 from .models import User, Rental, Review
 from . import db
@@ -54,6 +54,25 @@ def search_user():
                                                       .group_by(User.username)
                                                       )
                                    .all())
+
+                user_results = [None] * len(interim_results)
+                for i in range(len(interim_results)):
+                    user_results[i] = interim_results[i][0]
+            case 'most-date':
+                date = request.form.get("date")
+                rental_results = Rental.query.filter(Rental.date == date)
+                review_results = Review.query
+                user_count = (select(Rental.user.label("username"),func.count("*").label("user_count"))
+                                                .filter(Rental.date == date)
+                                                .group_by(Rental.user)
+                                                .order_by(desc(func.count("*")))
+                                                ).subquery()
+                max_user_count = db.session.execute(func.max(user_count.c.user_count)).scalar()
+                max_user_list = db.session.query(user_count).filter(user_count.c.user_count==max_user_count).subquery()
+                max_user_list_trimmed = select(max_user_list.c.username)
+                interim_results = (db.session.execute(select(User)
+                                                      .filter(User.username.in_(max_user_list_trimmed))
+                                                      )).all()
 
                 user_results = [None] * len(interim_results)
                 for i in range(len(interim_results)):
